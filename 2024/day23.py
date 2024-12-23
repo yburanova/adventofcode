0e2f4b068923d41f198c7c933a1bd91d2fd08f92
yneitzel <yulia.neitzel@gmail.com>
2024-12-23 07:21:11 +0100

    [2024][DAY 23] add solution 2

--- 2024/day23.py
+++ 2024/day23.py
@@ -39,4 +39,36 @@ def solve_part_i():
     three_comp_connections_with_t  =filter_with_t(three_comp_connections)
     print(f"Part I: {len(three_comp_connections_with_t)}")
 
-solve_part_i()
+
+def bron_kerbosch(connections_map, R, P, X, cliques):
+    if not P and not X:  # Found a maximal clique
+        cliques.append(R)
+        return
+    for v in list(P):
+        bron_kerbosch(connections_map, R.union([v]), P.intersection(connections_map[v]), X.intersection(connections_map[v]), cliques)
+        P.remove(v)
+        X.add(v)
+
+def find_all_comp_connections(connections):
+    connections_map = {}
+    for connection in connections:
+        comp1, comp2 = connection
+        connections_map[comp2] = connections_map.get(comp2, []) + [comp1]
+        connections_map[comp1] = connections_map.get(comp1, []) + [comp2]
+
+    all_nodes = set(connections_map.keys())
+    cliques = []
+    bron_kerbosch(connections_map, set(), all_nodes, set(), cliques)
+
+    # Step 3: Find the largest clique
+    largest_clique = max(cliques, key=len) if cliques else []
+    return sorted(largest_clique)
+
+def solve_part_ii():
+    connections_gen = read_file(filepath)
+    largest_comp_connection = find_all_comp_connections(connections_gen)
+    print(largest_comp_connection)
+    print(f"Part II: {','.join(largest_comp_connection)}")
+
+#solve_part_i()
+solve_part_ii()
