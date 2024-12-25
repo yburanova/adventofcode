964a1eaff88723f73027f449a9c45ddc3e840275
yneitzel <yulia.neitzel@gmail.com>
2024-12-25 07:42:33 +0100

    [2024][DAY 24] add solution 1

--- 2024/day24.py
+++ 2024/day24.py
@@ -26,16 +26,12 @@ def read_file(filepath):
 
 
 def all_z_set(current_stack, z_nodes):
-    current_stack_z_nodes = [wire for wire, value in current_stack.items() if wire.startswith('z')]
-    if len(current_stack_z_nodes) == len(z_nodes):
-        return True
-    else:
-        return False
+    return all(wire in current_stack for wire in z_nodes)
 
 def get_value(wire, gates, current_stack):
-    wire_value = current_stack.get(wire)
-    if wire_value is not None:
-        return wire_value
+    if wire in current_stack:
+        return current_stack[wire]
+
     else:
         gate = gates[wire]
         value1 = get_value(gate.in_node1, gates, current_stack)
@@ -51,29 +47,17 @@ def get_value(wire, gates, current_stack):
         current_stack[wire] = output
         return output
 
-
-def complete_wires(current_stack, gates, z_node):
-    get_value(z_node, gates, current_stack)
-
-
 def simulate_for_z(initial_values, gates, z_nodes):
     current_stack = initial_values
     for z_node in z_nodes:
-        complete_wires(current_stack, gates, z_node)
+        get_value(current_stack, gates, z_node)
         if all_z_set(current_stack, z_nodes):
             break
 
     return {node: value for node, value in current_stack.items() if node.startswith('z')}
 
 def return_binary(z_value_map):
-    sorted_map = {key: z_value_map[key] for key in sorted(z_value_map.keys())}
-    result = ''
-    for node, value in sorted_map.items():
-        result += str(value)
-
-    return result[::-1]
-
-
+    return ''.join(str(z_value_map[node]) for node in sorted(z_value_map.keys()))[::-1]
 
 
 def solve_part_i():
