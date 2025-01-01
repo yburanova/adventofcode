2a8de95d3fb7910f64f5d801e6702717a25594d8
yneitzel <yulia.neitzel@gmail.com>
2025-01-01 14:18:39 +0100

    [2024][DAY 2] add solution

--- 2024/day02.py
+++ 2024/day02.py
@@ -2,23 +2,49 @@ import numpy as np
 
 filepath = "day02.txt"
 
-file_reader_gen = (row for row in open(filepath))
+file_reader_gen = (row.split() for row in open(filepath))
+
+def get_nums(file_reader_gen):
+    nums_array = []
+    for row in file_reader_gen:
+        nums_array.append(np.array([int(i) for i in row]))
+
+    return nums_array
 
 def solve_part_I():
 
     result = 0
     for row in file_reader_gen:
-        nums = np.array([int(i) for i in row.split()])
-        diffs = np.diff(nums)
-
-        if (diffs > 0).all() and (diffs <= 3).all():
+        nums = np.array([int(i) for i in row])
+        if is_safe(nums):
             result += 1
 
-        if (diffs < 0).all() and (diffs >= -3).all():
+    print("Part I:", result)
+
+def is_safe(nums):
+    diffs = np.diff(nums)
+    if (diffs > 0).all() and (diffs <= 3).all():
+        return True
+
+    if (diffs < 0).all() and (diffs >= -3).all():
+        return True
+
+    return False
+
+def solve_part_ii():
+    nums_list = get_nums(file_reader_gen)
+    result = 0
+    for nums in nums_list:
+        if is_safe(nums):
             result += 1
+            continue
 
-    return result
+        for i in range(len(nums)):
+            nums_copy = np.delete(nums, i)
+            if is_safe(nums_copy):
+                result += 1
+                break
+    print("Part II", result)
 
-
-result_1 = solve_part_I()
-print(result_1)
+solve_part_I()
+solve_part_ii()
