2658e288dcc14eda92e9a583ac3c7b61be1428cd
yneitzel <yulia.neitzel@gmail.com>
2025-01-02 14:14:00 +0100

    [2024][DAY 4] add solution 2

--- 2024/day04.py
+++ 2024/day04.py
@@ -39,8 +39,36 @@ def solve_part_I():
         new_lines.append(new_line)
     result += transpose_and_search(new_lines)
 
-    print(result)
+    print("Part I:", result)
 
 
+def is_x_mas(pos_a, lines):
+    if pos_a[0] == 0 or pos_a[0] == len(lines) - 1 or pos_a[1] == 0 or pos_a[1] == len(lines[0]) - 1:
+        return False
+
+    diag_1 = f"{lines[pos_a[0] - 1][pos_a[1] - 1]}A{lines[pos_a[0] + 1][pos_a[1] + 1]}"
+    diag_2 = f"{lines[pos_a[0] + 1][pos_a[1] - 1]}A{lines[pos_a[0] - 1][pos_a[1] + 1]}"
+
+    if diag_1 in ['MAS', 'SAM'] and diag_2 in ['MAS', 'SAM']:
+        return True
+
+    return False
+
+
+def solve_part_ii():
+    lines = [line.strip() for line in open(filepath)]
+    all_a_positions = []
+    for i, row in enumerate(lines):
+        for j, sign in enumerate(row):
+            if sign == 'A':
+                all_a_positions.append((i,j))
+
+    result = 0
+    for pos_a in all_a_positions:
+        if is_x_mas(pos_a, lines):
+            result += 1
+
+    print("Part II:", result)
 
 solve_part_I()
+solve_part_ii()
